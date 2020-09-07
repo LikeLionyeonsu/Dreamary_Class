46a219d3c9135454416f4480c96b7ef57bea9cd3
LikeLion <skopr@likelion.org>
2020-09-07 23:30:03 +0900

    rr

--- page/models.py
+++ page/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Designer(models.Model):
-    image = models.ImageField(upload_to = 'images/')
+    image = models.ImageField(upload_to = 'media/')
     name = models.CharField(max_length = 50)
     address = models.CharField(max_length = 255)
     description = models.TextField()
